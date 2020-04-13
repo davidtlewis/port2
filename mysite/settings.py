4c9b991ef4e0d0c5518ae0829aa631771a7cbb06
davidtlewis <info@dtlewis.com>
2020-04-13 21:28:04 +0100

    moved from sqllite to microserver mysqll

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,12 +83,26 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portfolio',
+        'USER': 'portfolio',
+        'PASSWORD': 'mervan',
+        'HOST': '192.168.1.199',
+        'PORT': '3306',
+        
+    }
+}
+
 
 
 # Password validation
