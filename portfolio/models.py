f19d598351875b162658ac5ca09e07e2990a7f23
davidtlewis <info@dtlewis.com>
2020-04-18 18:31:08 +0100

    did linting

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,23 +1,25 @@
+import locale
+import time
+from datetime import datetime, date, timedelta
 from django.db import models
 from django.db.models import Sum
 import requests
 from bs4 import BeautifulSoup
-import locale
 from django.utils import timezone
 from django.urls import reverse
-from datetime import datetime, date, timedelta
-import time
-from pandas.tseries.offsets import BDay
+
+
 
 class Account(models.Model):
+    """ this holds the accounts"""
     name = models.CharField(max_length=50)
     ACCOUNT_TYPE = (
-        ('ISA','ISA'),
+        ('ISA', 'ISA'),
         ('pension', 'PENSION'),
-        ('standard','STANDARD'),
+        ('standard', 'STANDARD'),
     )
     account_type = models.CharField(max_length=8, choices=ACCOUNT_TYPE, default='buy')
-    account_value = models.DecimalField(max_digits=9, decimal_places=2, default = 0)
+    account_value = models.DecimalField(max_digits=9, decimal_places=2, default=0)
     person = models.ForeignKey('Person', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
@@ -30,13 +32,14 @@ class Account(models.Model):
     def refresh_value(self):
         holdings = Holding.objects.filter(account=self)
         account_value = holdings.aggregate(Sum('current_value'))['current_value__sum']
-        if account_value is None: account_value = 0
+        if account_value is None:
+            account_value = 0
         self.account_value = account_value
         self.save()
 
 class Person(models.Model):
     name = models.CharField(max_length=50)
-            
+
     def __str__(self):
         return self.name
 
@@ -49,15 +52,15 @@ class Stock(models.Model):
     yahoo_code = models.CharField(max_length=20, blank=True)
     nickname = models.CharField(max_length=40)
     CURRENCY_TYPE = (
-        ('gbp','GBP'),
-        ('gbx','GBX'),
-        ('usd','USD'),
+        ('gbp', 'GBP'),
+        ('gbx', 'GBX'),
+        ('usd', 'USD'),
     )
     currency = models.CharField(max_length=6, choices=CURRENCY_TYPE, default='equity')
     STOCK_TYPE = (
-        ('fund','FUND'),
-        ('equity','EQUITY'),
-        ('etfs','ETFS'),
+        ('fund', 'FUND'),
+        ('equity', 'EQUITY'),
+        ('etfs', 'ETFS'),
     )
     stock_type = models.CharField(max_length=6, choices=STOCK_TYPE, default='equity')
     current_price = models.DecimalField(max_digits=7, decimal_places=2)
@@ -73,7 +76,7 @@ class Stock(models.Model):
         return reverse('stock_detail', args=[str(self.id)])
 
     def refresh_value(self):
-        locale.setlocale(locale.LC_ALL,'en_US.UTF-8')
+        locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
         baseurl1 = "https://markets.ft.com/data/"
         baseurl2 = {
             "etfs":"etfs/tearsheet/performance?s=",
@@ -84,27 +87,29 @@ class Stock(models.Model):
         page = requests.get(url)
         contents = page.content
         soup = BeautifulSoup(contents, 'html.parser')
-        scrapped_current_price = soup.find_all("span",class_='mod-ui-data-list__value')[0].string
+        scrapped_current_price = soup.find_all("span", class_='mod-ui-data-list__value')[0].string
         current_price = locale.atof(scrapped_current_price)
-        if (self.currency == 'gbx'): current_price = current_price / 100
+        if self.currency == 'gbx':
+            current_price = current_price / 100
         #p = Price(stock = s, price= current_price)
         #p.save()
         self.current_price = current_price
         self.price_updated = timezone.now()
         self.save()
         #now create price history record
-        p = Price(stock = self, price= current_price)
+        p = Price(stock=self, price=current_price)
         p.save()
         #now to refresh related holdings !
-        related_holdings = Holding.objects.filter(stock=self) 
+        related_holdings = Holding.objects.filter(stock=self)
         for h in related_holdings:
             h.refresh_value()
 
 
     def get_historic_prices(self):
-        if not self.yahoo_code: return 
+        if not self.yahoo_code:
+            return
         batch = 135
-        locale.setlocale(locale.LC_ALL,'en_US.UTF-8')
+        locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
         def converttonumber(textin):
             try:
                 return locale.atof(textin)
@@ -119,12 +124,12 @@ class Stock(models.Model):
             last_date = date(2017, 1, 1)
         else:
             last_date = last_date_record.date
-        print ("today:", today, " ;last date:",  last_date)
+        print("today:", today, ", last date:", last_date)
 
         from_date = last_date + timedelta(days=1)
         while from_date < today:
             to_date = min((from_date + timedelta(days=batch)), today)
-            print ("from:", from_date, " to last date:",  to_date)           
+            print("from:", from_date, " to last date:", to_date)
             endunix = int(time.mktime(to_date.timetuple()))
             startunix = int(time.mktime(from_date.timetuple()))
             url = "https://uk.finance.yahoo.com/quote/" + self.yahoo_code + "/history?period1=" + str(startunix) + "&period2=" + str(endunix) + "&interval=1d&filter=history&frequency=1d"
@@ -133,46 +138,46 @@ class Stock(models.Model):
             contents = page.content
             soup = BeautifulSoup(contents, 'html.parser')
             rows = soup.table.tbody.find_all("tr")
-            print (len(rows))
+            print(len(rows))
             for table_row in rows:
                 columns = table_row.find_all("td")
                 if len(columns) == 7:
                     #save price record
-                    hp = HistoricPrice(stock = self, date=datetime.strptime(columns[0].text,'%d %b %Y'), open=converttonumber(columns[1].text), high=converttonumber(columns[2].text), low=converttonumber(columns[3].text), close=converttonumber(columns[4].text), adjclose=converttonumber(columns[5].text))
-                    hp.save()                   
+                    hp = HistoricPrice(stock=self, date=datetime.strptime(columns[0].text, '%d %b %Y'), open=converttonumber(columns[1].text), high=converttonumber(columns[2].text), low=converttonumber(columns[3].text), close=converttonumber(columns[4].text), adjclose=converttonumber(columns[5].text))
+                    hp.save()
                     #maybe use uniqueness of data to stop duplicate being added.
                 if len(columns) == 2:
                     #save div record
                     amount_text = columns[1].strong.text
-                    div = Dividend(stock = self, date=datetime.strptime(columns[0].text,'%d %b %Y'), amount=converttonumber(amount_text) )
-                    div.save() 
+                    div = Dividend(stock=self, date=datetime.strptime(columns[0].text, '%d %b %Y'), amount=converttonumber(amount_text))
+                    div.save()
             #get ready for next loop
             from_date = to_date + + timedelta(days=1)
 
-    
+
     def clear_historic_prices(self):
-        HistoricPrice.objects.filter(stock = self).delete()
-        Dividend.objects.filter(stock = self).delete()
+        HistoricPrice.objects.filter(stock=self).delete()
+        Dividend.objects.filter(stock=self).delete()
 
 class Dividend(models.Model):
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE, null=True)
-    date =  models.DateField(blank=True)
+    date = models.DateField(blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=8)
     class Meta:
         ordering = ['-date',]
-    
+
 class Transaction(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
     TRANSACTION_TYPE = (
-        ('buy','BUY'),
+        ('buy', 'BUY'),
         ('sell', 'SELL'),
     )
     transaction_type = models.CharField(max_length=4, choices=TRANSACTION_TYPE, default='buy')
     date = models.DateField()
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE, null=True)
     volume = models.IntegerField()
-    price = models.DecimalField(max_digits=5, decimal_places=2,default=0)
-    tcost = models.DecimalField(max_digits=5, decimal_places=2,default=0)
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    tcost = models.DecimalField(max_digits=5, decimal_places=2, default=0)
 
     class Meta:
         ordering = ['-date']
@@ -183,21 +188,21 @@ class Transaction(models.Model):
 
 class Price(models.Model):
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE, null=True)
-    date =  models.DateTimeField(auto_now_add=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
 
     class Meta:
-        ordering = ['-date','stock']
+        ordering = ['-date', 'stock']
 
     def __str__(self):
-        return (self.stock.name + " at " + str(self.date) )
+        return(self.stock.name + " at " + str(self.date))
 
 class HistoricPrice(models.Model):
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE, null=True)
-    date =  models.DateField(blank=True)
+    date = models.DateField(blank=True)
     open = models.DecimalField(max_digits=7, decimal_places=2)
     high = models.DecimalField(max_digits=7, decimal_places=2)
-    low  = models.DecimalField(max_digits=7, decimal_places=2)
+    low = models.DecimalField(max_digits=7, decimal_places=2)
     close = models.DecimalField(max_digits=7, decimal_places=2)
     adjclose = models.DecimalField(max_digits=7, decimal_places=2)
 
@@ -205,7 +210,7 @@ class HistoricPrice(models.Model):
         ordering = ['date']
 
     def __str__(self):
-        return (self.stock.name + " at " + str(self.date) )
+        return(self.stock.name + " at " + str(self.date))
 
 
 
@@ -213,15 +218,15 @@ class Holding(models.Model):
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE, null=True)
     account = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
     volume = models.IntegerField()
-    book_cost = models.DecimalField(max_digits = 10, decimal_places=2)
-    current_value = models.DecimalField(max_digits = 10, decimal_places=2)
+    book_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    current_value = models.DecimalField(max_digits=10, decimal_places=2)
     value_updated = models.DateTimeField(null=True)
 
     class Meta:
         ordering = ['stock']
 
     def __str__(self):
-        return (self.stock.name + " in " + str(self.account.name) )
+        return (self.stock.name + " in " + str(self.account.name))
 
     def get_absolute_url(self):
         return reverse('holding_detail', args=[str(self.id)])
@@ -229,26 +234,14 @@ class Holding(models.Model):
     def refresh_value(self):
         #counter =  Transaction.objects.filter(stock__name=self.stock).count()
         transactions = Transaction.objects.filter(stock=self.stock).filter(account=self.account)
-        nett_volume_bought = transactions.filter(transaction_type= 'buy').aggregate(Sum('volume'))['volume__sum']
-        if nett_volume_bought is None: nett_volume_bought = 0
-        
-        nett_volume_sold = transactions.filter(transaction_type= 'sell').aggregate(Sum('volume'))['volume__sum']
-        if nett_volume_sold is None: nett_volume_sold = 0
-         
-        self.volume = nett_volume_bought - nett_volume_sold
-        self.current_value = Price.objects.filter(stock=self.stock).latest('date').price * self.volume
-        self.value_updated = timezone.now()
-        self.save()
+        nett_volume_bought = transactions.filter(transaction_type='buy').aggregate(Sum('volume'))['volume__sum']
+        if nett_volume_bought is None:
+            nett_volume_bought = 0
 
-    def refresh_value_old(self):
-        #counter =  Transaction.objects.filter(stock__name=self.stock).count()
-        transactions = Transaction.objects.filter(stock=self.stock).filter(account=self.account)
-        nett_volume_bought = transactions.filter(transaction_type= 'buy').aggregate(Sum('volume'))['volume__sum']
-        if nett_volume_bought is None: nett_volume_bought = 0
-        
-        nett_volume_sold = transactions.filter(transaction_type= 'sell').aggregate(Sum('volume'))['volume__sum']
-        if nett_volume_sold is None: nett_volume_sold = 0
-         
+        nett_volume_sold = transactions.filter(transaction_type='sell').aggregate(Sum('volume'))['volume__sum']
+        if nett_volume_sold is None:
+            nett_volume_sold = 0
+
         self.volume = nett_volume_bought - nett_volume_sold
         self.current_value = Price.objects.filter(stock=self.stock).latest('date').price * self.volume
         self.value_updated = timezone.now()
