e6b460846d150b5df56d3aa4fd676f72fe037bba
davidtlewis <info@dtlewis.com>
2020-10-16 14:04:21 +0100

    not sure

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -9,8 +9,8 @@ class TransactionForm(forms.ModelForm):
         fields = ('account', 'transaction_type', 'date', 'stock', 'volume', 'price', 'tcost')
 
 class CommandForm(forms.Form):
-    do_get_prices  = forms.BooleanField(required=False)
-    do_refresh_accounts  = forms.BooleanField(required=False)
+    do_get_prices  = forms.BooleanField(required=False, initial=True )
+    do_refresh_accounts  = forms.BooleanField(required=False, initial=True)
     do_refresh_holdings  = forms.BooleanField(required=False)
     do_get_history = forms.BooleanField(required=False)
     do_get_perf  = forms.BooleanField(required=False)
