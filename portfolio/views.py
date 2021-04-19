dff3d8768fafc88129af68d56c0b64f3e2f94b8c
david <info@dtlewis.com>
2021-04-19 17:21:21 +0100

    adjusted home screen to include HCW pension

--- portfolio/views.py
+++ portfolio/views.py
@@ -178,7 +178,7 @@ def recalc(request):
 def custom_report(request):
     a = Account.objects.filter(person__name = "david") | Account.objects.filter(person__name = "henri")
     total = a.aggregate(Sum('account_value'))
-    dtl_pension = Account.objects.filter(person__name = "david", account_type = "pension") 
+    dtl_pension = Account.objects.filter(account_type = "pension") 
     a = Account.objects.filter(person__name = "david").exclude(account_type = "pension") | Account.objects.filter(person__name = "henri").exclude(account_type = "pension")
     accounts_by_type = a.values('account_type').annotate(total_value=Sum('account_value'))
     return render(request, 'portfolio/custom_report.html', {
