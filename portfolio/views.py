a775b77680efad0c6562fe09899606d9d2d5bdb7
David Lewis <info@dtlewis.com>
2021-06-21 19:13:54 +0100

    tweak context naming

--- portfolio/views.py
+++ portfolio/views.py
@@ -178,10 +178,10 @@ def recalc(request):
 def custom_report(request):
     a = Account.objects.filter(person__name = "david") | Account.objects.filter(person__name = "henri")
     total = a.aggregate(Sum('account_value'))
-    dtl_pension = Account.objects.filter(account_type = "pension") 
+    pensions = Account.objects.filter(account_type = "pension") 
     a = Account.objects.filter(person__name = "david").exclude(account_type = "pension") | Account.objects.filter(person__name = "henri").exclude(account_type = "pension")
     accounts_by_type = a.values('account_type').annotate(total_value=Sum('account_value'))
     stock_currencies = Stock.objects.filter(stock_type = "curr")
     return render(request, 'portfolio/custom_report.html', {
-    'total': total, 'dtl_pension':dtl_pension, 'accounts_by_type': accounts_by_type, 'stock_currencies': stock_currencies,
+    'total': total, 'pensions':pensions, 'accounts_by_type': accounts_by_type, 'stock_currencies': stock_currencies,
     }, )
