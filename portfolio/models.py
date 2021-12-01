6701ab2dea264afaf51d7eb06acbcd3b0f9da996
David Lewis <info@dtlewis.com>
2021-12-01 15:26:36 +0000

    tweak to yahoo scraper

--- portfolio/models.py
+++ portfolio/models.py
@@ -111,18 +111,18 @@ class Stock(models.Model):
             if self.stock_type == 'etfs' or self.stock_type =='curr':
                 url = "https://finance.yahoo.com/quote/" + self.yahoo_code
             session = HTMLSession() # trying new library to get more reliable scrapes
-            print(f"Calling URL: {url}")
+            #print(f"Calling URL: {url}")
             page = session.get(url)
             #page = requests.get(url)
             contents = page.content
             soup = BeautifulSoup(contents, 'html.parser')
             if self.stock_type == 'etfs' or self.stock_type =='curr':
-                scrapped_element =  soup.find("span", attrs={"data-reactid": "29"})
+                scrapped_element =  soup.find("fin-streamer", attrs={"data-reactid": "29"})
                 if scrapped_element is not None:
                         scrapped_current_price = scrapped_element.string
                         current_price = locale.atof(scrapped_current_price)
                 else:
-                    print("!!!!WARNING: Scrape fail")
+                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!WARNING: Scrape fail")
                     scrapped_current_price = ""
                     current_price = self.current_price
             else:
