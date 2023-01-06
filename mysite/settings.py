f403ec6b33b1e5c416c312a778e9dea3ed14701d
David Lewis <info@dtlewis.com>
2023-01-06 16:32:16 +0000

    adjusted home page to show date updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4n63e7wpm@knc6v1h^45t812+=e@z%l_s^p8)^s(#45)6*4kz2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.199','microserver',]
 
 USE_THOUSAND_SEPARATOR = True
 
